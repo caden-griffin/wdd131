7c3a3a880bb92db1f977913e778153d18f133e05
caden-griffin <cadengriffin0@gmail.com>
2026-05-23 07:45:49 -0600

    temples

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+
+});
+
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`
